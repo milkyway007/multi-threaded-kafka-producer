0f8fc3ca229839f52973ea7b60bb2c6843a52fc6
Liidia Laada <liidia.laada@finestmedia.ee>
2023-07-03 15:15:11 +0300

    Scripts

--- producer/scripts/topic-create.cmd
+++ producer/scripts/topic-create.cmd
@@ -1,1 +1,1 @@
-%KAFKA_HOME%\bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --topic nse-eod-topic --partitions 5 --replication-factor 3
+%KAFKA_HOME%\bin\windows\kafka-topics.bat --create --bootstrap-server localhost:9092 --topic nse-eod-topic --partitions 5 --replication-factor 3
